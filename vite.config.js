201ad29a77ca2e8cedf5b7990927a1a374f9dc81
Hughchen2046 <hughchen2046@gmail.com>
2025-12-03 14:11:01 +0800

    ok

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   //判斷 ? 產品 : 開發中
-  base: '/react-gh-pages/',
+   base: process.env.NODE_ENV === 'production' ? '/react-gh-pages/' : '/',
   plugins: [react()],
   server: {
     open: true,
